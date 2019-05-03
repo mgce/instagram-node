317708749f063497288eb3f20fd79fb994d48e80
Mateusz Gajda <matgajda@outlook.com>
2019-05-03 21:38:24 +0200

    added script to generate proto file to js

--- packages/user-service/src/index.ts
+++ packages/user-service/src/index.ts
@@ -1,6 +1,17 @@
-import {App} from "@instagram-node/common"
+const grpc = require('grpc')
+const protoLoader = require('@grpc/proto-loader')
 
-const PORT = 3000;
-const app = new App(PORT);
+const PROTO_PATH = './src/proto/user.proto'
+const SERVER_URI = '0.0.0.0:5001'
 
-app.listen();
+const packageDefinition = protoLoader.loadSync(PROTO_PATH)
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition)
+
+// we'll implement the handlers here
+
+const server = new grpc.Server()
+server.addService(protoDescriptor.UserService.service, {})
+server.bind(SERVER_URI, grpc.ServerCredentials.createInsecure())
+
+server.start()
+console.log('Server is running!')
---
+++ packages/user-service/src/proto/genProto.sh
@@ -0,0 +1,13 @@
+
+# It must be a relative path to the protoc-gen-ts
+# Path to this plugin 
+PROTOC_GEN_TS_PATH=".\node_modules\.bin\protoc-gen-ts.cmd"
+ 
+# Directory to write generated code to (.js and .d.ts files) 
+OUT_DIR="."
+ 
+protoc \
+    --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
+    --js_out="import_style=commonjs,binary:${OUT_DIR}" \
+    --ts_out="service=true:${OUT_DIR}" \
+    *.proto
