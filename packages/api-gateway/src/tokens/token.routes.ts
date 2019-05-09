330e0fe4947f7eb2ff57daafcdb715c287cc5bc0
Mateusz Gajda <matgajda@outlook.com>
2019-05-09 19:02:50 +0200

    Add loading existing refreshToken

--- packages/api-gateway/src/tokens/token.routes.ts
+++ packages/api-gateway/src/tokens/token.routes.ts
@@ -6,18 +6,18 @@ import { UserClient } from "../users/user.client";
 import { authenticateValidator } from "./token.validators";
 import { RefreshTokenModel } from "./refreshToken.model";
 import { RefreshToken } from "./refreshToken.dto";
-import { getRepository } from 'typeorm';
+import { getRepository, MoreThan } from 'typeorm';
 var randtoken = require('rand-token');
 
 const tokenRouter = express.Router();
 
-tokenRouter.post('/', authenticateValidator, requestValidator, function (req: express.Request, res: express.Response) {
+tokenRouter.post('/', authenticateValidator, requestValidator, async (req: express.Request, res: express.Response) => {
     const { emailAddress, password } = req.body;
     const request = new AuthenticateRequest();
     request.setEmailaddress(emailAddress);
     request.setPassword(password);
 
-    UserClient.authenticate(request, (err, result: AuthenticateResponse) => {
+    UserClient.authenticate(request, async (err, result: AuthenticateResponse) => {
         if (err)
             return res.send(err);
 
@@ -28,24 +28,30 @@ tokenRouter.post('/', authenticateValidator, requestValidator, function (req: ex
 
             const token = jwt.sign(claims, secret, { expiresIn: 36000 })
             //first, check if refreshToken exist
-            const rt = refreshTokenRepository.createQueryBuilder().where(`expirationDate > ${new Date()}`).execute();
-
-            //create new refresh token
-            const expirationDate = new Date();
-            expirationDate.setDate(expirationDate.getDate() + 30);
-            const refreshToken : RefreshToken = {
-                refreshToken: randtoken.uid(256),
-                expirationDate: expirationDate,
+            let refreshToken = await refreshTokenRepository
+            .createQueryBuilder()
+            .where({
+                expirationDate: MoreThan(new Date()),
                 userId: claims.userid
+            })
+            .getOne();
+
+            if(!refreshToken)
+            {
+                const expirationDate = new Date();
+                expirationDate.setDate(expirationDate.getDate() + 30);
+                refreshToken = refreshTokenRepository.create({
+                    refreshToken: randtoken.uid(256),
+                    expirationDate: expirationDate,
+                    userId: claims.userid
+                });
+                await refreshTokenRepository.save(refreshToken);
             }
 
-            const entity = refreshTokenRepository.create(refreshToken);
-            refreshTokenRepository.save(entity).then(() => {
-                res.json({
-                    success: true,
-                    token: `Bearer ${token}`,
-                    refreshToken: refreshToken.refreshToken
-                })
+            res.json({
+                success: true,
+                token: `Bearer ${token}`,
+                refreshToken: refreshToken.refreshToken
             })
         }
         else
